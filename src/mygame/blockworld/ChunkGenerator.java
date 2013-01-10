3d4bd039298372553a958952fceaa11fbc286196
Fusion <Fusion@Fusion_Desktop>
2013-01-10 09:30:19 +0100

    Improved generated terrain

--- src/mygame/blockworld/ChunkGenerator.java
+++ src/mygame/blockworld/ChunkGenerator.java
@@ -18,10 +18,11 @@ public class ChunkGenerator {
     }
           
     public void fillChunk(Chunk cnk) { 
+        float plateau = r.nextFloat()*4.5f;
         Noise noise = new Noise(cnk.CHUNK_SIZE);
         for (int x = cnk.fXC; x < cnk.fXC + cnk.CHUNK_SIZE; x++) {
             for (int z = cnk.fZC; z < cnk.fZC + cnk.CHUNK_SIZE; z++) {
-                int calculatedHeight = Math.round(noise.getMap()[x-cnk.fXC][z-cnk.fZC]);
+                float calculatedHeight = (noise.getMap()[x-cnk.fXC][z-cnk.fZC])* plateau;
                 for (int y = cnk.fYC; y < cnk.fYC + cnk.CHUNK_SIZE; y++) {       
                     if (y < calculatedHeight) {
                         cnk.addBlock(1, x, y, z);
